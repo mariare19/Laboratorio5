73c51f772ca8388b6a08febe5a4223e99dae725f
Maria Reneé Palma <mariare_19@hotmail.com>
2018-10-25 19:48:57 -0600

    cambios por retroalimentacion del lab5

--- myapp/routes/foodie.js
+++ myapp/routes/foodie.js
@@ -2,61 +2,82 @@ var express = require('express');
 var router = express.Router();
 var method = require('../methods/methods');
 
-router.get('/read/:id?', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
+router.get('/:id?', function (req, res) {
 	if (req.params.id) {
 		method.getFoodieRecipe(req.params.id).then(response => {
 			if (response.length > 0) {
-				res.json(response)
+				res.status(200).json(response);
 			} else {
-				res.sendStatus(404)
+				res.status(404).send();
 			}
-		}).catch(error => console.error(error));
+		}).catch(err => {
+			if (res.headersSent) {
+				return next(err);
+			}
+			res.status(500).json({ error: err });
+		});
 	} else {
 		method.getFoodieRecipes().then(response => {
 			if (response.length > 0) {
-				res.json(response)
+				res.status(200).json(response)
 			} else {
-				res.sendStatus(404)
+				res.status(404).send();
 			}
-		}).catch(error => console.error(error));
+		}).catch(err => {
+			if (res.headersSent) {
+				return next(err);
+			}
+			res.status(500).json({ error: err });
+		});
 	}
 });
 
-router.delete('/delete/:id', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
+router.delete('/:id', function (req, res) {
 	method.deleteFoodieRecipe(req.params.id).then(response => {
 		if (response.result.ok) {
-			res.sendStatus(204);
+			res.status(204).send();
 		} else {
-			res.sendStatus(404);
+			res.status(404).send();
 		}
-	}).catch(error => console.error(error));
+	}).catch(err => {
+		if (res.headersSent) {
+			return next(err);
+		}
+		res.status(500).json({ error: err });
+	});
 });
 
-router.put('/update/:id', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
+router.put('/:id', function (req, res) {
 	method.updateFoodieRecipe(req.params.id, req.body).then(response => {
 		if (response.result.ok) {
-			res.sendStatus(204);
+			res.status(204).send();
 		} else {
-			res.sendStatus(404);
+			res.status(404).send();
 		}
-	}).catch(error => console.error(error));
+	}).catch(err => {
+		if (res.headersSent) {
+			return next(err);
+		}
+		res.status(500).json({ error: err });
+	});
 });
 
-router.post('/create', function (req, res) {
-	res.setHeader('Content-Type', 'application/json');
+router.post('', function (req, res) {
 	if (req.headers["content-type"] == 'application/json') {
 		method.createFoodieRecipe(req.body).then(response => {
 			if (response.result.ok) {
-				res.sendStatus(201);
+				res.status(201).send();
 			} else {
-				res.sendStatus(404);
+				res.status(404).send();
 			}
-		}).catch(error => console.error(error));
+		}).catch(err => {
+			if (res.headersSent) {
+				return next(err);
+			}
+			res.status(500).json({ error: err });
+		});
 	} else {
-		res.sendStatus(404);
+		res.status(404).send();
 	}
 });
 
